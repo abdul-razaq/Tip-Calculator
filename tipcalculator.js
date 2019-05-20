cfd16f018c4f38f8b635f3a8f032d209e402db09
AbdulRazaq Suleiman <nivvy.nivans@gmail.com>
2019-05-20 12:15:39 +0100

    Tip Calculator Project

---
+++ tipcalculator.js
@@ -0,0 +1,40 @@
+// Calculate the Tip
+function calculateTip() {
+
+    let billAmt = document.getElementById("billamt").value;
+    let serviceQual = document.getElementById("serviceQual").value;
+    let numOfPeople = document.getElementById("peopleamt").value;
+
+    // Validate input
+    if (billAmt === "" || serviceQual === 0) {
+        alert("Please enter valid values!");
+        return;
+    }
+
+    // check to see if this input is empty or less than or equal to 1
+    if (numOfPeople === "" || numOfPeople <= 1) {
+        numOfPeople = 1;
+        document.getElementById("each").style.display = "none";
+    } else {
+        document.getElementById("each").style.display = "block";
+    }
+
+    // Calculate Tip
+    let total = (billAmt * serviceQual) / numOfPeople;
+    // round to 2 decimal places
+    total = Math.round(total * 100) / 100;
+    // allows us to always have two digits after decimal point
+    total = total.toFixed(2);
+    // Display the Tip
+    document.getElementById("totalTip").style.display = "block";
+    document.getElementById("each").innerHTML = total;
+}
+
+// Hide the tip amount on load
+document.getElementById("totalTip").style.display = "none";
+document.getElementById("each").style.display = "none";
+
+// Click to call function
+document.getElementById("calculate").onclick = () => {
+    calculateTip();
+};
